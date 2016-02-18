618f0c935045abc7c70f3484cc373dea5a586c62
Thomas Naude <thomas.naude@gmail.com>
2016-02-18 11:36:54 +0100

    created models

---
+++ app/models/dose.rb
@@ -0,0 +1,7 @@
+class Dose < ActiveRecord::Base
+  belongs_to :ingredient
+  belongs_to :cocktail
+  validates :description, presence: true
+  validates :cocktail, presence: true
+  validates :ingredient, presence: true, uniqueness: { scope: :cocktail }
+end
